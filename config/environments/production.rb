cb6c1fd7d0bff271ec2c62c11760656a033c142d
Chnadrakant Solanki <chandrkant.solanki@stellingtech.com>
2017-05-18 08:16:27 +0530

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   # config.log_level = :debug
-  config.logger.level = Logger::WARN
+  config.level = Logger::WARN
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
