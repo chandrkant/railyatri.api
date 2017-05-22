644f554f032020125c7a60ca40fa395888a7b08a
Chnadrakant Solanki <chandrkant.solanki@stellingtech.com>
2017-05-22 22:42:22 +0530

    change

--- app/controllers/api/train_controller.rb
+++ app/controllers/api/train_controller.rb
@@ -4,7 +4,7 @@ class Api::TrainController < ApplicationController
 	def time_table
 		begin
 		 result ={}
-	   url = API_URLS['time_table']+"#{params[:train_number]}.json?stop=true"
+	   url = "https://test.railyatri.in/m/schedule/"+"#{params[:train_number]}.json?stop=true"
 		 req = RestClient.get(url)
 		 json_response(req)
 		rescue Exception => e
@@ -19,7 +19,7 @@ class Api::TrainController < ApplicationController
 	def live_arr_dep
 		begin
 		 result ={}
-		 url = API_URLS['live_arr_dep']
+		 url = "https://livestatus.railyatri.in/api/train_at_station/"
 		if params[:to_code].present?
 			url = url+"#{params[:from_code]}/#{params[:to_code]}/8.json"
 		else
@@ -37,7 +37,7 @@ class Api::TrainController < ApplicationController
 
   #GET /api/train_bw_station
 	def train_bw_station
-		url = API_URLS['train_bw_station']
+		url = "https://test.railyatri.in/api/trains-between-station.json"
 		url = url+"?from=#{params[:from_code]}&to=#{params[:to_code]}"
 		req = RestClient.get(url)
 		json_response(req)
@@ -62,7 +62,7 @@ class Api::TrainController < ApplicationController
 
   #POST /api/instant_platform_update
   def instant_platform_update
-  	url = API_URLS['update_platform']
+  	url = "https://test.railyatri.in/instant_platform_update.json"
 		req = RestClient.post url, params.to_json, {content_type: :json, accept: :json}
 		json_response(req)
   end
